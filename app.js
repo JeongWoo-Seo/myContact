cf1e61ae1ec0063716c6f8a5ee12515a5726f051
JeongWoo-Seo <sjwoo6500@naver.com>
2025-04-09 17:51:48 +0900

    install modules method-override, urlencoded,ejs,dotenv

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 import express from 'express';
 import contractsRoutes from './routes/contactsRoutes.js';
+import methodOverRide from 'method-override'
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('./public'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverRide('_method'));
 
 app.use('/',contractsRoutes);
 
